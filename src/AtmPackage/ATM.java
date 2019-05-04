89eef5fd3ad9867db0b6be2f10b5c46673fa38e7
DenisKantic <denis.kantic18@gmail.com>
2019-05-04 13:34:11 +0200

    created default constructor

--- src/AtmPackage/ATM.java
+++ src/AtmPackage/ATM.java
@@ -2,4 +2,20 @@ package AtmPackage;
 
 public class ATM {
 
+
+    private int id;
+    private double balance;
+
+    public ATM() {
+
+        this.id=0;
+        this.balance=100.0;
+    }
+
+    public ATM(int id, double balance) {
+        this.id = id;
+        this.balance = balance;
+    }
+
+
 }
