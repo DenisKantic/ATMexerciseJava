e1829e2a958e84b4aca298c962af93f84eeacb53
DenisKantic <denis.kantic18@gmail.com>
2019-05-05 08:07:38 +0200

    Created switchMethod method in Main

--- src/AtmPackage/Main.java
+++ src/AtmPackage/Main.java
@@ -1,10 +1,14 @@
 package AtmPackage;
 
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
 
+        ATM[] atm = new ATM[10];
 
+        initialBalance(atm);
 
 
     }
@@ -29,8 +33,64 @@ public class Main {
                 "\n4. exit");
     }
 
+    public void switchMethod(int id, ATM[] acc, int choise) {
 
+        switch (choise) {
 
+            case 1:
 
+                System.out.println("Your account balance is: " + acc[id].getBalance());
+
+                break;
+
+            case 2:
+
+                Scanner s = new Scanner(System.in);
+
+                System.out.println("How much would you like to deposit?");
+
+                double value = s.nextDouble();
+
+                if (value > 0) {
+
+                    acc[id].deposit(value);
+
+                    System.out.println("Deposit transfer completed.");
+                } else {
+
+                    System.out.println("Wrong input. Try again");
+                }
+
+                break;
+
+            case 3:
+
+                System.out.println("How much would you like to withdraw?");
+
+                Scanner input = new Scanner(System.in);
+                double withdraw = input.nextDouble();
+
+                if (withdraw - acc[id].getBalance() > 0) {
+
+                    acc[id].withdraw(withdraw);
+
+                    System.out.println("Withdraw transfer completed.");
+                    System.out.println("Your account balance is: " + acc[id].getBalance());
+                } else {
+
+                    System.out.println("Wrong input. Try again");
+                }
+                break;
+
+            case 4:
+
+                System.out.println("Thank you for using our service");
+
+                break;
+
+            default:
+
+                System.out.println("Wrong input. Try again");
+        }
     }
 }
