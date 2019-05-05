5ddbf852ebf1b507e443684cb3e0107f8ce1aae5
DenisKantic <denis.kantic18@gmail.com>
2019-05-05 08:00:59 +0200

    Created showMenu method

--- src/AtmPackage/Main.java
+++ src/AtmPackage/Main.java
@@ -3,6 +3,34 @@ package AtmPackage;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+
+
+
+
+    }
+
+
+    public static void initialBalance(ATM[] acc) {
+
+        double balance = 100.0;
+
+        for (int i = 0; i < acc.length; i++) {
+
+            acc[i] = new ATM(i, balance);
+        }
+    }
+
+    public void showMenu() {
+
+        System.out.print("Welcome to ATM Menu. Choose your option: " +
+                "\n1. check balance" +
+                "\n2. deposit" +
+                "\n3. withdraw" +
+                "\n4. exit");
+    }
+
+
+
+
     }
 }
